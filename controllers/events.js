a3036fc853dee146632834031b6533931436f2b9
sebbby <seb.beitel@gmail.com>
2020-10-28 15:02:00 -0400

    add committed players and recorded rounds to events page

--- controllers/events.js
+++ controllers/events.js
@@ -1,14 +1,54 @@
 const Event = require('../models/event');
 const User = require('../models/user');
+const Round = require('../models/round');
+const { all } = require('../routes/events');
 
 module.exports = {
   index
 };
 
 function index(req, res) {
-  Event.find({}).sort('date').exec(function(err, events) {
-    User.findById(req.user._id).populate('rounds').exec(function(err, user) {
-      res.render('events/index', { events, user });
+  User.find({}).populate('rounds').exec(function(err, allUsers){
+    Event.find({}).sort('date').exec(function(err, events) {
+      User.findById(req.user._id).populate('rounds').exec(function(err, user) {
+        let committed = getCommitted(allUsers);
+        let recorded = getRecorded(allUsers, events);
+        let utcDate = new Date()
+        res.render('events/index', { events, user, utcDate, committed, recorded });
+      });
     });
-  });
+  })  
 };
+
+function getCommitted(allUsers) {
+  let committed = {}
+  allUsers.forEach(u => {
+    u.events.forEach(e => {
+      if (committed[e]) {
+        committed[e].push(u.firstName)  
+      } else {
+        committed[e] = [u.firstName]
+      }
+    })
+  })
+  return committed
+}
+
+function getRecorded(allUsers, events) {
+  let recorded = {}
+  allUsers.forEach(u => {
+    events.forEach(e => {
+      if (recorded[e._id]) {
+        if (u.rounds.findIndex(r => r.event.equals(e._id)) > -1) {
+          recorded[e._id].push([u.firstName, u.rounds[u.rounds.findIndex(r => r.event.equals(e._id))].grossScore, u.rounds[u.rounds.findIndex(r => r.event.equals(e._id))].netScore])  
+        }
+      } else {
+        if (u.rounds.findIndex(r => r.event.equals(e._id)) > -1) {
+          recorded[e._id] = [[ u.firstName, u.rounds[u.rounds.findIndex(r => r.event.equals(e._id))].grossScore, u.rounds[u.rounds.findIndex(r => r.event.equals(e._id))].netScore ]]
+        }
+      }
+    })
+  })
+  return recorded
+}
+
